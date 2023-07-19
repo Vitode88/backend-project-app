3361b27cef62521743317b77e49249073c95fb7d
Aitor Ilie <aitorilie@MacBook-Air-de-Aitor.local>
2023-07-19 10:25:44 +0200

    created trip interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import connectDB from "./database";
 import "./loadEnvironment";
+import startServer from "./server/startServer";
 
 const mongoURL = process.env.MONGODB_URL;
+const port = +process.env.PORT || 3100;
 
 (async () => {
     try {
         await connectDB(mongoURL);
+        await startServer(port);
     } catch (error) {
         process.exit(1);
     }
---
+++ src/server/controllers/tripsController.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+
+import CustomError from "../../errors/CustomError";
+
+export const getAllTrips = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const products = await ProductModel.find();
+        res.status(200).json({ products });
+    } catch (error) {
+        const finalError = new CustomError(
+            400,
+            "Error loading products.",
+            "Error loading products."
+        );
+        next(finalError);
+    }
+};
