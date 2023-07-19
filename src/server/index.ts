d3b293ed0000e283eed5680ef6011be1442af89d
Aitor Ilie <aitorilie@MacBook-Air-de-Aitor.local>
2023-07-19 19:26:17 +0200

    routes created

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,14 @@
 import morgan from "morgan";
 import express from "express";
+import { TripModel } from "../database/models/TripsSchema";
+import tripsRouter from "./routes/tripsRouter";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/trips", tripsRouter);
 
 export default app;
